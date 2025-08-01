034ee3ef62be72e9065500e19f19be2535fa108b
Hilman Nuril Hadi <hilmannuril@gmail.com>
2025-08-01 17:09:23 +0700

    Update index.php
    test pertama

--- index.php
+++ index.php
@@ -111,6 +111,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" name="subject_key" placeholder="subject key" required>
         <input type="text" name="username" placeholder="Enter username" required>
         <button type="submit">Login</button>
+        <h2>test saja</h2>
     </form>
 </body>
 </html>
